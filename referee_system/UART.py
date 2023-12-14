9d1bf962bdeca249e452392bfc85241e0836edd5
yigehanhanZK <1692302759@qq.com>
2023-12-14 21:25:21 +0800

    version 0.1

---
+++ referee_system/UART.py
@@ -0,0 +1,381 @@
+import serial
+import numpy as np
+import offical_Judge_Handler
+import binascii  # 在BIN(二进制)和ASCII之间转换
+import struct  # 将数据作为完整的结构传输，用struct模块进行处理
+from binascii import *
+from crcmod import *
+from offical_Judge_Handler import crc16Add
+import time
+
+buffercnt = 0
+buffer = [0]
+buffer *= 1000
+cmdID = 0
+indecode = 0
+
+
+def read(ser):
+    global buffercnt
+    buffercnt = 0
+    global buffer
+    global cmdID
+    global indecode
+    # TODO:qt thread
+
+    while True:
+        s = ser.read(1)
+        s = int().from_bytes(s, 'big')
+        # doc.write('s: '+str(s)+'        ')
+
+        if buffercnt > 50:
+            buffercnt = 0
+
+        print(buffercnt)
+        buffer[buffercnt] = s
+        # doc.write('buffercnt: '+str(buffercnt)+'        ')
+        # doc.write('buffer: '+str(buffer[buffercnt])+'\n')
+        print(hex(buffer[buffercnt]))
+
+        if buffercnt == 0:
+            if buffer[buffercnt] != 0xa5:
+                buffercnt = 0
+                continue
+
+        if buffercnt == 5:
+            if offical_Judge_Handler.myVerify_CRC8_Check_Sum(id(buffer), 5) == 0:
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 7:
+            cmdID = (0x0000 | buffer[5]) | (buffer[6] << 8)
+            print("cmdID")
+            print(cmdID)
+
+        if buffercnt == 10 and cmdID == 0x0002:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 10):
+                Referee_Game_Result()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 20 and cmdID == 0x0001:
+
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 20):
+                # 比赛阶段信息
+                UART_passer.Referee_Update_GameData()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 41 and cmdID == 0x0003:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 41):
+                # 各车血量
+                UART_passer.Referee_Robot_HP()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 12 and cmdID == 0x0004:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 12):
+                Referee_dart_status()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 13 and cmdID == 0x0101:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 13):
+                Referee_event_data()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 13 and cmdID == 0x0102:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 13):
+                Refree_supply_projectile_action()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 11 and cmdID == 0x0104:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 11):
+                Refree_Warning()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 10 and cmdID == 0x0105:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 10):
+                Refree_dart_remaining_time()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 17 and cmdID == 0x301:  # 2bite数据
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 17):
+                # 比赛阶段信息
+                UART_passer.Receive_Robot_Data()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 25 and cmdID == 0x202:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 25):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 25 and cmdID == 0x203:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 25):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 27 and cmdID == 0x201:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 27):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 10 and cmdID == 0x204:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 10):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 10 and cmdID == 0x206:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 10):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 13 and cmdID == 0x209:  # 雷达没有
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 13):
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        if buffercnt == 16 and cmdID == 0x0301:
+            if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 16):
+                # Refree_map_stop()
+                buffercnt = 0
+                if buffer[buffercnt] == 0xa5:
+                    buffercnt = 1
+                continue
+
+        # if buffercnt == 24 and cmdID == 0x0303:
+        #     if offical_Judge_Handler.myVerify_CRC16_Check_Sum(id(buffer), 24):
+        #         # 云台手通信
+        #         Refree_Arial_Message()
+        #         buffercnt = 0
+        #         if buffer[buffercnt] == 0xa5:
+        #             buffercnt = 1
+        #         continue
+
+        buffercnt += 1
+
+
+enemy = 0
+
+
+class Robomst_UART:
+    debug = True
+    home_width = 9.3
+    home_height = 4.65
+    real_width = 28
+    real_height = 15
+
+    def __init__(self):
+        self.robot_location = None
+        self.Id_red = 1
+        self.Id_blue = 101
+        self.specific_color = {0: [1, 2, 3, 4, 5, 7], 1: [101, 102, 103, 104, 105, 107]}
+        self.between_car_len = b'\x0a'
+        self.send_id = 9 if enemy else 109
+
+    def create_SOF(self, datalen):
+        buffer = [0]
+        buffer = buffer * 5
+        buffer[0] = 0xa5
+        buffer[1] = datalen
+        buffer[2] = 0
+        buffer[3] = 0
+        buffer[4] = offical_Judge_Handler.myGet_CRC8_Check_Sum(id(buffer), 4, 0xff)
+
+        return bytes(bytearray(buffer))
+
+    def push_loc(self, location):
+        self.robot_location = np.float32(location)
+
+    def get_position(self):
+        return self.robot_location
+
+    def xy_check(self, x: float, y: float, ):
+        new_x = x * self.real_width / self.home_width
+        new_y = y * self.real_height / self.home_height
+
+        return new_x, new_y
+
+    def radar_map(self, ID, X, Y, ser, whether_new=False):
+        Z = 1
+        try:
+            SOF = (b'\xa5' b'\x0a' b'\x00' b'\x00' b'\x37') if whether_new else (
+                b'\xa5' b'\x0e' b'\x00' b'\x00' b'\x37')
+            CMDID = (b'\x05' b'\x03')
+            data = struct.pack("<1H2f", ID, X, Y) if whether_new else struct.pack("<1H3f", ID, X, Y, Z)
+            data1 = SOF + CMDID + data
+            decodeData = binascii.b2a_hex(data1).decode('utf-8')
+            data_last, hexer = crc16Add(decodeData)
+            ser.write(hexer)
+
+        except Exception as e:
+            print("serial write data has ERROR: \033[0m", e)
+
+    def radar_map_test(self, ID, X, Y, ser):
+        try:
+            Z = 1
+            SOF = self.create_SOF(14)
+            CMDID = (b'\x05' b'\x03')
+            data = struct.pack("<1H3f", ID, X, Y, Z)
+            data1 = SOF + CMDID + data
+            decodeData = binascii.b2a_hex(data1).decode('utf-8')
+            data_last, hexer = crc16Add(decodeData)
+            ser.write(hexer)
+        except:
+            print(f'The ID:{ID}, and the X:{X} and the Y:{Y}')
+
+    def radar_between_car(self, data: list, data_id: int, datalenth: int, receiver_id, ser):
+        SOF = self.create_SOF(datalenth + 6)  # datalength 指的是我要发的数据长度，前面还有6位的字节漂移
+        CMDID = (b'\x01' b'\x03')
+        # data = struct.pack("<4f", data[0], data[1], data[2], data[3])
+        data = bytes(bytearray(data))
+        dataid_sender_receiver = struct.pack('<3H', data_id, self.send_id, receiver_id)
+        data_sum = SOF + CMDID + dataid_sender_receiver + data
+        decodeData = binascii.b2a_hex(data_sum).decode('utf-8')
+        data_last, hexer = crc16Add(decodeData)
+        ser.write(hexer)
+
+    def Referee_Transmit_BetweenCar(self, dataID, ReceiverId, data, ser):
+        """
+        雷达站发送车间通信包函数
+        """
+        buffer = [0]
+        buffer = buffer * 200
+
+        buffer[0] = 0xA5  # 数据帧起始字节，固定值为 0xA5
+        buffer[1] = 10  # 数据帧中 data 的长度,占两个字节
+        buffer[2] = 0
+        buffer[3] = 0  # 包序号
+        buffer[4] = offical_Judge_Handler.myGet_CRC8_Check_Sum(id(buffer), 5 - 1, 0xff)  # 帧头 CRC8 校验
+        buffer[5] = 0x01
+        buffer[6] = 0x03
+        # 自定义内容ID
+        buffer[7] = dataID & 0x00ff
+        buffer[8] = (dataID & 0xff00) >> 8
+        # 发自雷达站
+        if enemy:
+            buffer[9] = 9
+        else:
+            buffer[9] = 109
+        buffer[10] = 0
+        buffer[11] = ReceiverId
+        buffer[12] = 0
+        # 自定义内容数据段
+        buffer[13] = data[0]
+        buffer[14] = data[1]
+        buffer[15] = data[2]
+        buffer[16] = data[3]
+
+        offical_Judge_Handler.Append_CRC16_Check_Sum(id(buffer), 10 + 9)  # 等价的
+
+        buffer_tmp_array = [0]
+        buffer_tmp_array *= 9 + 10
+
+        for i in range(9 + 10):
+            buffer_tmp_array[i] = buffer[i]
+        ser.write(bytearray(buffer_tmp_array))
+
+    def Referee_Transmit_Map(self, cmdID, datalength, targetId, x, y, ser):
+        """
+        小地图包
+
+        x，y采用np.float32转换为float32格式
+        """
+        buffer = [0]
+        buffer = buffer * 200
+
+        buffer[0] = 0xA5  # 数据帧起始字节，固定值为 0xA5
+        buffer[1] = (datalength) & 0x00ff  # 数据帧中 data 的长度,占两个字节
+        buffer[2] = ((datalength) & 0xff00) >> 8
+        buffer[3] = 0  # 包序号
+        buffer[4] = offical_Judge_Handler.myGet_CRC8_Check_Sum(id(buffer), 5 - 1, 0xff)  # 帧头 CRC8 校验
+        buffer[5] = cmdID & 0x00ff
+        buffer[6] = (cmdID & 0xff00) >> 8
+
+        buffer[7] = targetId
+        buffer[8] = 0
+        buffer[9] = bytes(x)[0]
+        buffer[10] = bytes(x)[1]
+        buffer[11] = bytes(x)[2]
+        buffer[12] = bytes(x)[3]
+        buffer[13] = bytes(y)[0]
+        buffer[14] = bytes(y)[1]
+        buffer[15] = bytes(y)[2]
+        buffer[16] = bytes(y)[3]
+        buffer[17:20] = [0] * 4  # 朝向，直接赋0，协议bug，不加这一项无效
+
+        offical_Judge_Handler.Append_CRC16_Check_Sum(id(buffer), datalength + 9)  # 等价的
+
+        buffer_tmp_array = [0]
+        buffer_tmp_array *= 9 + datalength
+
+        for i in range(9 + datalength):
+            buffer_tmp_array[i] = buffer[i]
+        ser.write(bytearray(buffer_tmp_array))
+
+    def Robot_Data_Transmit_Map(self, ser, pred_loc):
+        for row in pred_loc:
+            target_id = int(row[0])
+            if target_id in self.specific_color[enemy]:
+                x, y = float(row[1]), float(row[2])
+                x, y = self.xy_check(x, y)
+                self.radar_map_test(target_id, x, y, ser)
+                time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    ser = serial.Serial('/dev/ttyUSB0', 115200, 8, 'N', 1, timeout=0.01)
+
+    pred_loc = np.array([[7, 4, 3],
+                         [1, 7, 1]])
+    uart = Robomst_UART()
+    # uart.Robot_Data_Transmit_Map(ser, pred_loc)
+
+    dataID = 0x020F
+    receive_id = 103
+
+    while 1:
+        # read(ser=ser)
+        # uart.Referee_Transmit_BetweenCar(dataID=dataID, ReceiverId=receive_id, data=[0, 1, 2, 3], ser=ser)
+        # time.sleep(0.1)
+        # uart.Robot_Data_Transmit_Map(ser, pred_loc)
+        uart.radar_between_car(data=[0, 8, 9, 3], data_id=dataID, datalenth=4, receiver_id=receive_id, ser=ser)
+        time.sleep(0.1)
+
---
+++ referee_system/static_uart.py
@@ -0,0 +1,228 @@
+import serial
+import binascii  # 在BIN(二进制)和ASCII之间转换
+import struct  # 将数据作为完整的结构传输，用struct模块进行处理
+from binascii import *
+from crcmod import *
+import time
+import offical_Judge_Handler
+import numpy as np
+import copy
+import threading
+import random
+
+from macro import home_test, position_alarm, enemy, receiver_id
+
+
+def is_inside(box: np.ndarray, point: np.ndarray):
+    """
+    判断点是否在凸四边形中
+    :param box:为凸四边形的四点 shape is (4,2)
+    :param point:为需判断的是否在内的点 shape is (2,)
+    """
+    assert box.shape == (4, 2)
+    assert point.shape == (2,)
+    AM = point - box[0]
+    AB = box[1] - box[0]
+    BM = point - box[1]
+    BC = box[2] - box[1]
+    CM = point - box[2]
+    CD = box[3] - box[2]
+    DM = point - box[3]
+    DA = box[0] - box[3]
+    a = np.cross(AM, AB)
+    b = np.cross(BM, BC)
+    c = np.cross(CM, CD)
+    d = np.cross(DM, DA)
+    return a >= 0 and b >= 0 and c >= 0 and d >= 0 or \
+        a <= 0 and b <= 0 and c <= 0 and d <= 0
+
+
+class Static_UART:
+    if home_test:
+        home_width = 9.3
+        home_height = 4.65
+    else:
+        home_width = 28
+        home_height = 15
+    real_width = 28
+    real_height = 15
+    specific_color = {0: [1, 2, 3, 4, 5, 7], 1: [101, 102, 103, 104, 105, 107]}
+    _lock = threading.Lock()
+    stop_flag = False
+    robot_location = None
+    alarm_flag = 0
+    alarm_location = None
+    alarm_enemy = ['enemy_is_red', 'enemy_is_blue'][enemy]
+    send_id = 9 if enemy else 109
+    data_id = 0x020F
+    receiver = receiver_id[enemy]
+
+    @staticmethod
+    def create_SOF(datalen):
+        """
+        创建一个帧头（SOF），其中包含了长度信息、校验和等数据，以便用于数据传输
+        """
+        buffer = [0]
+        buffer = buffer * 5
+        buffer[0] = 0xa5
+        buffer[1] = datalen
+        buffer[2] = 0
+        buffer[3] = 0
+        buffer[4] = offical_Judge_Handler.myGet_CRC8_Check_Sum(id(buffer), 4, 0xff)  # 校验值
+
+        return bytes(bytearray(buffer))
+
+    @staticmethod
+    def push_loc(location):
+        """
+        将传入的位置信息 location 深复制到类变量 Static_UART.robot_location 中
+        """
+        # Static_UART._lock.acquire()
+        # 如果不适用深复制（或许浅复制也行），那么多进程时可能反而会更慢
+        Static_UART.robot_location = copy.deepcopy(location)
+        # Static_UART._lock.release()
+
+    @staticmethod
+    def push_alarm(location):
+        """
+        将传入的位置信息 location 深复制到类变量 Static_UART.alarm_location  中
+        """
+        Static_UART.alarm_location = copy.deepcopy(location)
+        Static_UART.alarm_flag = 1
+
+    @staticmethod
+    def radar_between_car(data: list, datalenth: int, receiver_id, ser):
+        """
+        将指定的数据通过串口 ser 传输给雷达设备
+        """
+        SOF = Static_UART.create_SOF(datalenth + 6)  # datalength 指的是我要发的数据长度，前面还有6位的字节漂移
+        CMDID = (b'\x01' b'\x03')
+        data = bytes(bytearray(data))  # 将列表转换为字节流
+        dataid_sender_receiver = struct.pack('<3H', Static_UART.data_id, Static_UART.send_id, receiver_id)
+        data_sum = SOF + CMDID + dataid_sender_receiver + data
+        decodeData = binascii.b2a_hex(data_sum).decode('utf-8')  # 将 data_sum 转换为十六进制表示，并通过 decode('utf-8') 将其解码为字符串
+        data_last, hexer = offical_Judge_Handler.crc16Add(decodeData)
+        # data_last: 附加了 CRC-16 校验码后的完整数据
+        # hexer: 附加了 CRC-16 校验码的完整数据的二进制表示
+        ser.write(hexer)
+
+    @staticmethod
+    def random_receiver(whether_random):
+        """
+        根据 whether_random 参数来决定是否随机选择接收者
+        """
+        if whether_random:
+            return random.choice(Static_UART.receiver)
+        else:
+            return Static_UART.receiver[0]
+
+    @staticmethod
+    def radar_map(ID, X, Y):
+        Z = 1
+        try:
+            SOF = Static_UART.create_SOF(14)
+            CMDID = (b'\x05' b'\x03')
+            data = struct.pack("<1H3f", ID, X, Y, Z)  # 按照格式 "<1H3f"打包为二进制数据
+            data1 = SOF + CMDID + data
+            decodeData = binascii.b2a_hex(data1).decode('utf-8')  # 转换为16进制表示，又将其解码成字符串
+            _, hexer = offical_Judge_Handler.crc16Add(decodeData)
+            # hexer: 附加了 CRC-16 校验码的完整数据的二进制表示
+            return hexer
+
+        except Exception as e:
+            print("serial write data has ERROR: \033[0m", e)
+
+    @staticmethod
+    def xy_check(x: float, y: float, ):
+        """
+        将传入坐标（x,y,）转换为相对于真实场地的坐标值x,y
+        """
+        new_x = x * Static_UART.real_width / Static_UART.home_width
+        new_y = y * Static_UART.real_height / Static_UART.home_height
+
+        return new_x, new_y
+
+    @staticmethod
+    def alarm_xy_check(numpy_xy):
+        """
+        将传入坐标（numpy）转换为相对于真实场地的坐标值(numpy)
+        """
+        new_x = numpy_xy[0] * Static_UART.real_width / Static_UART.home_width
+        new_y = numpy_xy[1] * Static_UART.real_height / Static_UART.home_height
+        return np.array([new_x, new_y])
+
+    @staticmethod
+    def Robot_Data_Transmit_Map(ser):
+        """
+        通过串口传输位置信息，判断是否报警
+        """
+        try:
+            for row in Static_UART.robot_location:
+                target_id = int(row[0])
+                if target_id in Static_UART.specific_color[enemy]:
+                    x, y = float(row[1]), float(row[2])
+                    # check_xy 之后获得 真实场地的xy
+                    x, y = Static_UART.xy_check(x, y)
+                    print(x, y)
+                    hexer = Static_UART.radar_map(target_id, x, y)
+                    ser.write(hexer)  # 将生成的数据 hexer（包含id,坐标）通过串口 ser 进行传输
+                    for alarm in position_alarm[Static_UART.alarm_enemy]:
+                        # 检查当前目标ID是否在报警相关数据中，并调用 is_inside() 函数判断机器人的位置是否在报警区域内
+                        if target_id in alarm[0] and is_inside(np.array(alarm[1]),
+                                                               Static_UART.alarm_xy_check(row[1:3])):
+                            data = Static_UART.handle_id(target_id) + Static_UART.handle_id(alarm[-1])
+                            print(data)
+                            # print(Static_UART.random_receiver(False if home_test else True))
+                            Static_UART.radar_between_car(data, datalenth=4,
+                                                          receiver_id=Static_UART.random_receiver(
+                                                              104 if home_test else True), ser=ser)
+                time.sleep(0.1)
+        except:
+            # print(Static_UART.robot_location)
+            time.sleep(0.1)
+
+    @staticmethod
+    def handle_id(target_id):
+        if target_id > 100:
+            target_id -= 100
+        if target_id == 1:
+            target_id = [1, 0]
+        if target_id == 2:
+            target_id = [2, 0]
+        if target_id == 3:
+            target_id = [2, 1]
+        if target_id == 4:
+            target_id = [2, 2]
+        if target_id == 5:
+            target_id = [3, 2]
+
+        return target_id
+
+    @staticmethod
+    def advanced_loop(ser):
+        while 1:
+            Static_UART.Robot_Data_Transmit_Map(ser)
+            if Static_UART.stop_flag:
+                break
+
+
+if __name__ == "__main__":
+    import threading
+    import copy
+
+    ser = serial.Serial('/dev/ttyUSB0', 115200, 8, 'N', 1, timeout=0.01)
+    pred_loc1 = np.array([
+        [104, 2.8346, 0.06]])
+    alarm_thread = threading.Thread(target=Static_UART.advanced_loop, args=(ser,))
+    alarm_thread.start()
+
+    # counter = 0
+    # whole_pred = [pred_loc1, pred_loc2]
+    # Static_UART.push_loc(pred_loc1)
+
+    while 1:
+        Static_UART.push_loc(pred_loc1)
+        # Static_UART.push_alarm(pred_loc1)
+        # Static_UART.Robot_map_debug(ser)
+        # Static_UART.radar_between_car([2, 3, 1, 0], datalenth=4, receiver_id=104, ser=ser)
+
