63244bccb4e59ddd486f24de3649f83dc32728a8
yigehanhanZK <1692302759@qq.com>
2023-12-26 15:53:22 +0800

    添加保存图像操作

--- debug/debug_camera.py
+++ debug/debug_camera.py
@@ -1,5 +1,5 @@
 import cv2
-
+import os
 from camera import mvsdk
 from camera.camera import CameraThread, tune_exposure
 
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     tune_exposure(ht.cap, ht._date, high_reso=True)
     cv2.namedWindow("test", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("test", 1280, 960)
-    # mvsdk.CameraReadParameterFromFile(ht.cap.hCamera, '')
+
+    count = 0
+    save_folder = '../save_stuff/photos/'
     while 1:
         ret, frame = ht.read()
         cv2.imshow('test', frame)
@@ -17,5 +19,9 @@ if __name__ == "__main__":
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
+        if key == ord('s'):
+            filename = os.path.join(save_folder, f'save_{count}.jpg')
+            cv2.imwrite(filename, frame)
+            count += 1
     ht.release()
     cv2.destroyAllWindows()
