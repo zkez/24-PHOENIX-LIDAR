785f0021801cc6990723ec8cd8501bf9af56e7b1
yigehanhanZK <1692302759@qq.com>
2024-03-09 22:20:34 +0800

    add: 完善debug逻辑

--- debug/debug_detect.py
+++ debug/debug_detect.py
@@ -1,9 +1,13 @@
 import ctypes
 import os
 import shutil
+import time
+
 import cv2
 from macro import PLUGIN_LIBRARY, car_engine_file_path, armor_engine_file_path
-from detect.detect import YoLov8TRT, armor_post_process
+from detect.detect import YoLov8TRT
+from detect.detect_common import armor_post_process
+from camera.camera import HTCamera, CameraThread
 
 if __name__ == "__main__":
     ctypes.CDLL(PLUGIN_LIBRARY)
@@ -15,29 +19,35 @@ if __name__ == "__main__":
 
     YOLOv8_car = YoLov8TRT(car_engine_file_path)
     YOLOv8_armor = YoLov8TRT(armor_engine_file_path)
+    cap = CameraThread(0)
+    for i in range(10):
+        car_batch_image_raw, use_time_car, *a_car = YOLOv8_car.infer(YOLOv8_car.get_raw_image_zeros())
+        armor_batch_image_raw, use_time_armor, *a_armor = YOLOv8_armor.infer(YOLOv8_armor.get_raw_image_zeros())
 
     try:
-        video_path = "../detect/images/15.mp4"
-        cap = cv2.VideoCapture(video_path)
-        frame_width = int(cap.get(3))
-        frame_height = int(cap.get(4))
-
-        armor_location_list = []
         while True:
             ret, frame = cap.read()
-            if not ret:
+            if ret:
+                car_image, use_time_car, car_boxes, car_scores, car_classID, car_location \
+                    = YOLOv8_car.infer([frame])
+                for i in range(len(car_boxes)):
+                    box = car_boxes[i]
+                    img = frame[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+                    armor_image, use_time_armor, armor_boxes, armor_scores, armor_classID, armor_location \
+                        = YOLOv8_armor.infer([img])
+                    armor_post_process(armor_location, box)
+                    print(armor_location)
+                    for j in range(len(armor_boxes)):
+                        cv2.rectangle(frame, (int(armor_location[j][10]), int(armor_location[j][11])),
+                                              (int(armor_location[j][12]), int(armor_location[j][13])), (0, 255, 0), 2)
+                        cv2.putText(frame, categories[int(armor_location[j][9])], (int(armor_location[j][10]),
+                                                                              int(armor_location[j][11])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        cv2.imwrite('output/{}.jpg'.format(time.time()), frame)
+
+                    print('time->{:.2f}ms, fps->{}'.format((use_time_car + use_time_armor) * 1000,
+                                                                      1 / (use_time_car + use_time_armor)))
+            else:
                 break
-            car_image, use_time_car, car_boxes, car_scores, car_classID, car_location \
-                = YOLOv8_car.infer([frame])
-            for i in range(len(car_boxes)):
-                box = car_boxes[i]
-                img = frame[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
-                armor_image, use_time_armor, armor_boxes, armor_scores, armor_classID, armor_location \
-                    = YOLOv8_armor.infer([img])
-
-                armor_post_process(armor_location, box)
-
-                armor_location_list.append(armor_location)
 
     finally:
         YOLOv8_car.destroy()
