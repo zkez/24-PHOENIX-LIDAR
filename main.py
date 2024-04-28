bcb49fc5b9dadbc7361e908acc42142e7cf8cb40
yigehanhanZK <1692302759@qq.com>
2024-04-28 19:08:48 +0800

    add: 串口权限设置

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ctypes
 import serial
 import traceback
 import threading
-import pexpect
+import subprocess
 from process.radarprocess import RadarProcess
 from macro import position_choice, PLUGIN_LIBRARY
 from referee_system.static_uart import StaticUART, ReadUART
@@ -13,25 +13,28 @@ if __name__ == '__main__':
     try:
         ctypes.CDLL(PLUGIN_LIBRARY)
 
-        # TODO: 串口权限问题
-        # password = '608'
-        # ch = pexpect.spawn('sudo chmod 777 /dev/ttyUSB0')
-        # ch.sendline(password)
-        ser = serial.Serial('/dev/ttyUSB0', 115200, 8, 'N', 1, timeout=0.01)
+        # 串口权限设置
+        password = '608'
+        chmod_command = ['sudo', '-S', 'chmod', '777', '/dev/ttyUSB0']
+        subprocess.run(chmod_command, input=password+'\n', encoding='utf-8')
+        try:
+            ser = serial.Serial('/dev/ttyUSB0', 115200, 8, 'N', 1, timeout=0.1)
+        except serial.SerialException as e:
+            print("串口打开失败:", e)
+            exit()
 
+        # 主进程
         main_process = RadarProcess()
-        choice = position_choice if isinstance(position_choice, str) else input(
-            'Get new position? Y/y for yes, N/n for no\n')
 
+        # 裁判系统通信线程
         uart_thread = threading.Thread(target=StaticUART.advanced_loop, args=(ser, ), name='uart')
         read_thread = threading.Thread(target=ReadUART.read, args=(ser, ), name='read')
-        # alarm_thread = threading.Thread(target=StaticUART.alarm_loop, args=(ser, ), name='alarm')
         uart_thread.start()
         read_thread.start()
-        # alarm_thread.start()
 
+        choice = position_choice if isinstance(position_choice, str) else input(
+            'Get new position? Y/y for yes, N/n for no\n')
         if choice in ['Y', 'y']:
-            # main_process.panel.set_cam()
             main_process.get_position_new()
         elif choice in ['N', 'n']:
             main_process.get_position_using_last()
