046cd41fa4f1b585408f8e0eae715dcacade18fb
yigehanhanZK <1692302759@qq.com>
2024-04-28 19:36:23 +0800

    renew: pnp四点赛场确认

--- macro.py
+++ macro.py
@@ -5,7 +5,7 @@ debug = True
 CAMERA_CONFIG_DIR = 'Camera_config'
 CACHE_CONFIG_SAVE_DIR = 'save_stuff'
 preview_location = [(100, 100)]
-PC_STORE_DIR = '../save_stuff/points'
+PC_STORE_DIR = 'save_stuff/points'
 LIDAR_TOPIC_NAME = '/livox/lidar'
 LOCATION_SAVE_DIR = 'save_stuff/position'
 
@@ -17,9 +17,9 @@ armor_locations = []
 VIDEO_SAVE_DIR = 'save_stuff/video'
 MAP_PATH = 'save_stuff/output_map.jpg'
 
-PLUGIN_LIBRARY = "../../tensorrtx/yolov8/build/libmyplugins.so"
-car_engine_file_path = "../../tensorrtx/yolov8/build/yolov8m_car.engine"
-armor_engine_file_path = "../../tensorrtx/yolov8/build/yolov8s_armor.engine"
+PLUGIN_LIBRARY = 'detect/infer/libmyplugins.so'
+car_engine_file_path = "detect/infer/yolov8m_car.engine"
+armor_engine_file_path = "detect/infer/yolov8s_armor.engine"
 CONF_THRESH_CAR = 0.5
 CONF_THRESH_ARMOR = 0.01
 IOU_THRESHOLD = 0.4
@@ -45,18 +45,19 @@ location_targets = {
             'blue_outpost': [],
             'red_outpost': [],
             'blue_base': [],
-            'r_rt': [1.196, 6.376, 0.598],  # 插座往前数十格（烂的不算）
+            'r_rt': [1.196, 6.376, 0.598],
             'r_lt': [0.598, 6.376, 0.598],
             'b_rt': [],
             'b_lt': []
         },
-    'game':  # 按照官方手册填入
+    'game':  # 按照官方手册填入-24赛季相机位姿估计标记点
         {
-            # 24赛季相机位姿估计标记点
-            'red_base': [1.760, 7.539, 0.200 + 0.920],  # red base
-            'blue_outpost': [16.776, 12.565, 1.760],  # blue outpost
-            'red_outpost': [11.176, 2.435, 1.760],  # red outpost
-            'blue_base': [26.162, 7.539, 0.200 + 0.920],  # blue base
+            # 前哨战顶部 基地飞镖引导灯
+            'red_base': [1.760, 7.500, 1.043],  # red base
+            'blue_outpost': [16.816, -2.421 + 15.0, 1.586],  # blue outpost
+            'red_outpost': [11.184, -12.579 + 15.0, 1.586],  # red outpost
+            'blue_base': [26.240, 7.500, 1.043],  # blue base
+            # 狗洞附近的定位标签的左上角和右上角
             'r_rt': [8.670, -5.715 - 0.400 + 15., 0.420],  # r0 right_top
             'r_lt': [8.670, -5.715 + 15., 0.420],  # r0 left_top
             'b_rt': [19.330, -9.285 + 0.400 + 15., 0.420],  # b0 right_top
